65d4d10e356544018fb1c47c3b4ac6c8ff25fb7e
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-03-28 13:17:52 +0000

    fix news image responsiveness

--- src/components/ui/news-card.tsx
+++ src/components/ui/news-card.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function NewsCard({ data }: { data: any }) {
     return <div className="max-w-full mx-auto bg-white rounded-xl border overflow-hidden">
         <div className="flex flex-col">
-            <div className="md:flex-shrink-0 rounded-lg  relative w-full h-28 ">
-                {data?.banner_image && <Image className="w-full h-full" src={data.banner_image} alt="News Image" fill />}
+            <div className="md:flex-shrink-0 rounded-lg  relative w-full h-28">
+                {data?.banner_image && <Image className="w-full h-full object-cover" src={data.banner_image} alt="News Image" fill />}
             </div>
             <div className="px-2 py-4 border-t">
                 <h4 className="block mt-1 text-lg leading-tight font-medium text-clepher hover:underline line-clamp-3">{data.title}</h4>
